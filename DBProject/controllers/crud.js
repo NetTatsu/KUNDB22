62897f1c53fd81fc0cd814e5ef61d3d783be5b87
Lunarltn <103559017+Lunarltn@users.noreply.github.com>
2022-06-06 22:26:12 +0900

    post.pay추가

--- DBProject/controllers/crud.js
+++ DBProject/controllers/crud.js
@@ -4,11 +4,12 @@ exports.save = (req, res) => {
     const title = req.body.title;
     const category = req.body.category;
     const address = req.body.address;
+    const pay = req.body.pay;
     const contents = req.body.contents;
     const userid = req.body.userid;
 
     db.query('INSERT INTO post SET ?',
-    { title: title, category: category, address: address, contents: contents, userid: userid }
+    { title: title, category: category, address: address, pay:pay, contents: contents, userid: userid }
     , (error, results) => {
         if(error){
             console.log(error);
@@ -23,8 +24,9 @@ exports.update = (req, res) => {
     const title = req.body.title;
     const category = req.body.category;
     const address = req.body.address;
+    const pay = req.body.pay;
     const contents = req.body.contents;
-    db.query('UPDATE post SET ? WHERE postid= ?', [{ title:title, category:category, address:address, contents:contents }, postid]
+    db.query('UPDATE post SET ? WHERE postid= ?', [{ title:title, category:category, address:address, pay:pay, contents:contents }, postid]
     , (error, results) =>{
     if(error){
         console.log(error);
